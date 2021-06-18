0f3553090bcca30a404842801c42bac315daa083
David Ackerly <dackerly@berkeley.edu>
2021-06-18 11:09:34 -0700

    update ladder fuels example code

--- LadderFuels.R
+++ LadderFuels.R
@@ -1,6 +1,13 @@
 # preparing for TBC3 2021 meeting
 
+vbose <- FALSE
 #test code for syncing with DA
 x <- 5
 y <- 2
+if (vbose) print(x-y)
 print(x+y)
+
+Nplots <- 50
+for (i in 1:Nplots) {
+  if (vbose) print(i)
+}
