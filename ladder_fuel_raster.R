f7d7c01ab96562c56c7cbb50b0e66880634c104e
David Ackerly <dackerly@berkeley.edu>
2021-06-19 13:59:26 -0700

    Added 64m raster ladder fuels, script to query, and results in 'data' directory. Script calls on local files on DA computer so won't run for MK

---
+++ ladder_fuel_raster.R
@@ -0,0 +1,35 @@
+## reading in ladder fuel data
+require('raster')
+require('maptools')
+require('sp')
+require('rgeos')
+
+ld <- raster('big_data/Classified_Ladder_Fuels/ladder.tif')
+ld
+plot(ld)
+proj4string(ld)
+
+PWD <- readShapeSpatial('/Users/david/Google\ Drive/Drive-Projects/Pepperwood/PWD_GIS/PPshapefiles/PPshapefile-teale-albers/pepperwood')
+plot(PWD)
+proj4string(PWD) <- CRS('+proj=aea +datum=NAD83 +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000')
+
+PWDl <- spTransform(PWD,proj4string(ld))
+plot(ld)
+lines(PWDl)
+
+Pld <- crop(ld,PWDl)
+plot(Pld)
+lines(PWDl)
+
+p54 <- readShapeSpatial('/Users/david/Google\ Drive/Drive-Projects/Pepperwood/PepperwoodVegPlots-DAproject/plot.info/2021\ updated\ shapefiles\ and\ coordinates/shapefiles/vegplots-54-20m-aea/vegplots-54-20m')
+proj4string(p54) <- proj4string(PWD)
+p54l <- spTransform(p54,proj4string(ld))
+
+plot(Pld)
+plot(p54l,bg="transparent", add=TRUE)
+
+ldf <- extract(Pld,p54l)
+str(ldf)
+ldf
+
+ldf.sum
