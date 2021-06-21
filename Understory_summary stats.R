d5c7293949e7a98db2e6b120a5ea38e0f3a8bde9
melina_kozanitas <mkozanitas@gmail.com>
2021-06-21 10:48:06 -0700

    adding a summary stats script and resulting seed/juv csv

---
+++ Understory_summary stats.R
@@ -0,0 +1,27 @@
+
+seedling.juvenile<-get.seju.data(year=2013)
+#look at results
+head(seedling.juvenile)
+dim(seedling.juvenile)
+sum(seedling.juvenile$Total.Number) 
+
+write.csv(seedling.juvenile, "/Users/melinakozanitas/R projects/ppw_working/seju.data.csv", row.names=F)
+
+
+#not sure how to break these up by quad, plants.by.plot() is an existing function 
+
+tree<-plants.by.plot(year=2013, type="TR")
+sapling<-plants.by.plot(year=2013,type="SA")
+all<-plants.by.plot(year=2013,type="SA.TR")
+babies<-plants.by.plot(year = 2013, type = "SEJU")
+
+
+#from Megan's code...looks the same but totals remove quad distinction
+
+seju.data<-get.seju.data(2013)
+seju.data<-aggregate(cbind(Num.Seedlings, Num.Juveniles,Total.Number)~Species+Plot,   data=seju.data, FUN=sum)
+seju.data<-seju.data[,c(2,1,3,4,5)]
+
+
+
+
