18236040afea25aa0add8343b1f851c877d55b83
David Ackerly <dackerly@berkeley.edu>
2021-06-23 14:33:13 -0700

    new understory_summary_stats

--- scripts/Understory_summary stats.R
+++ scripts/Understory_summary stats.R
@@ -5,6 +5,25 @@ source('scripts/PWFunctions_load.R')
 
 source('scripts/PW_functions_local.R')
 
+# Setup master dataframe for plot level analyses
+plot.list <- get.plot(2013)
+pl <- data.frame(order=1:50)
+head(pl)
+pl$order
+
+pl$Plot=plot.list
+head(pl)
+
+ld <- read.csv('data/mean_ladder_fuel_54.csv')
+dim(ld)
+head(ld)
+
+pl$cl <- ld$ld[1:50]
+pl$rl <- ld$rl[1:50]
+head(pl)
+
+plot(pl$rl,pl$cl)
+
 #Extract and summarize saplings
 sapling<-plants.by.plot(year=2013,type="SA")
 #all<-plants.by.plot(year=2013,type="SA.TR")
@@ -15,46 +34,169 @@ sapling.p <- aggregate(cbind(Basal.Area, Count)~Plot, data=sapling, FUN=sum)
 head(sapling.p)
 dim(sapling.p)
 
-plot.list <- get.plot(2013)
-
-match.plots <- match(plot.list,sapling.p$Plot)
+match.plots <- match(pl$Plot,sapling.p$Plot)
 sapling.p <- cbind(plot.list,sapling.p[match.plots,])
-sapling.p <- sapling.p[,-2]
-names(sapling.p)[1] <- 'Plot'
-rownames(sapling.p) <- 1:50
 head(sapling.p)
-write.csv(sapling.p,'data/saplings_p.csv')
+sapling.p$Basal.Area[which(is.na(sapling.p$Basal.Area))] <- 0
+sapling.p$Count[which(is.na(sapling.p$Count))] <- 0
+head(sapling.p)
 
-# Extract and summarize trees
-tree<-plants.by.plot(year=2013, type="TR")
-head(tree)
+head(pl)
+pl$sapling.count <- sapling.p$Count
+pl$sapling.BA <- sapling.p$Basal.Area
+head(pl)
 
-## extract small trees
+# extract conifer counts and basal areas
+head(sapling)
+dim(sapling)
+conifer.sapling <- sapling[which(sapling$Species %in% c('PSEMEN','TORCAL')),]
+dim(conifer.sapling)
+
+conifer.sapling.p <- aggregate(cbind(Basal.Area, Count)~Plot, data=conifer.sapling, FUN=sum)
+head(conifer.sapling.p)
+dim(conifer.sapling.p)
+
+match.plots <- match(pl$Plot,conifer.sapling.p$Plot)
+conifer.sapling.p <- cbind(plot.list,conifer.sapling.p[match.plots,])
+head(conifer.sapling.p)
+dim(conifer.sapling.p)
+
+conifer.sapling.p$Basal.Area[which(is.na(conifer.sapling.p$Basal.Area))] <- 0
+conifer.sapling.p$Count[which(is.na(conifer.sapling.p$Count))] <- 0
+head(conifer.sapling.p)
+
+head(pl)
+pl$consap.count <- conifer.sapling.p$Count
+pl$consap.BA <- conifer.sapling.p$Basal.Area
+head(pl)
+
+# extract shrub counts and basal areas
+head(sapling)
+dim(sapling)
+table(sapling$Species)
+
+# four unknown saplings to possible clean up later
+
+# check shrub species list for future years
+shrub.sapling <- sapling[which(sapling$Species %in% c('HETARB','CEACUN','FRACAL','ARCMAN','AMOCAL','BACPIL','RHACRO','SAMNIG','QUEBER','QUEBEGA','PRUILI','PRUCER','CORCOR','CEATHY','ADEFAS','CERBET','HOLDIS')),]
+dim(shrub.sapling)
+head(shrub.sapling)
+
+shrub.sapling.p <- aggregate(cbind(Basal.Area, Count)~Plot, data=shrub.sapling, FUN=sum)
+head(shrub.sapling.p)
+dim(shrub.sapling.p)
+
+match.plots <- match(pl$Plot,shrub.sapling.p$Plot)
+shrub.sapling.p <- cbind(plot.list,shrub.sapling.p[match.plots,])
+head(shrub.sapling.p)
+dim(shrub.sapling.p)
+
+shrub.sapling.p$Basal.Area[which(is.na(shrub.sapling.p$Basal.Area))] <- 0
+shrub.sapling.p$Count[which(is.na(shrub.sapling.p$Count))] <- 0
+head(shrub.sapling.p)
+
+head(pl)
+pl$shrubsap.count <- shrub.sapling.p$Count
+pl$shrubsap.BA <- shrub.sapling.p$Basal.Area
+head(pl)
+
+pl$htsap.count <- pl$sapling.count - pl$consap.count - pl$shrubsap.count
+pl$htsap.BA <- pl$sapling.BA - pl$consap.BA - pl$shrubsap.BA
+head(pl)
+
+## DONE WITH SAPLINGS!!
+
+## MOVE ON TO TREES, by size class
 indv.data <- get.indv.data(year = 2013)
 head(indv.data)
 dim(indv.data)
 
-diam.range <- c(1,5) #enter min and max diam of interest
+diam.range <- c(7,10) #enter min and max diam of interest
 ba.range <- pi*(diam.range/2)^2
 
 small.trees <- indv.data[which(indv.data$Basal.Area>=ba.range[1] & indv.data$Basal.Area<ba.range[2]),]
 dim(small.trees)
+table(small.trees$Species)
 
 small.trees.p <- aggregate(Basal.Area~Plot,data=small.trees,FUN=sum)
 head(small.trees.p)
-
-plot.list <- get.plot(2013)
+dim(small.trees.p)
 
 match.plots <- match(plot.list,small.trees.p$Plot)
 small.trees.p <- cbind(plot.list,small.trees.p[match.plots,])
-small.trees.p <- small.trees.p[,-2]
-names(small.trees.p)[1] <- 'Plot'
-rownames(small.trees.p) <- 1:50
 head(small.trees.p)
 
-out.file <- paste('data/trees_',diam.range[1],'_',diam.range[2],'_p.csv',sep='')
-write.csv(small.trees.p,out.file)
+small.trees.p$Basal.Area[which(is.na(small.trees.p$Basal.Area))] <- 0
+head(small.trees.p)
 
+colname <- paste('TBA_',diam.range[1],'_',diam.range[2],sep='')
+pl$x <- small.trees.p$Basal.Area
+head(pl)
+names(pl)[grep('x',names(pl))] <- colname
+head(pl)
+
+## pull out conifers
+indv.data.archive <- indv.data
+
+
+# subset to conifers
+indv.data <- indv.data.archive[which(indv.data.archive$Species %in% c('PSEMEN','TORCAL')),]
+
+diam.range <- c(7,10) #enter min and max diam of interest
+ba.range <- pi*(diam.range/2)^2
+
+small.trees <- indv.data[which(indv.data$Basal.Area>=ba.range[1] & indv.data$Basal.Area<ba.range[2]),]
+dim(small.trees)
+table(small.trees$Species)
+
+small.trees.p <- aggregate(Basal.Area~Plot,data=small.trees,FUN=sum)
+head(small.trees.p)
+dim(small.trees.p)
+
+match.plots <- match(plot.list,small.trees.p$Plot)
+small.trees.p <- cbind(plot.list,small.trees.p[match.plots,])
+head(small.trees.p)
+
+small.trees.p$Basal.Area[which(is.na(small.trees.p$Basal.Area))] <- 0
+head(small.trees.p)
+
+colname <- paste('conTBA_',diam.range[1],'_',diam.range[2],sep='')
+pl$x <- small.trees.p$Basal.Area
+head(pl)
+names(pl)[grep('x',names(pl))] <- colname
+head(pl)
+
+# subset to shrubs
+indv.data <- indv.data.archive[which(indv.data.archive$Species %in% c('HETARB','CEACUN','FRACAL','ARCMAN','AMOCAL','BACPIL','RHACRO','SAMNIG','QUEBER','QUEBEGA','PRUILI','PRUCER','CORCOR','CEATHY','ADEFAS','CERBET','HOLDIS')),]
+dim(indv.data)
+head(indv.data)
+
+diam.range <- c(1,3) #enter min and max diam of interest
+ba.range <- pi*(diam.range/2)^2
+
+small.trees <- indv.data[which(indv.data$Basal.Area>=ba.range[1] & indv.data$Basal.Area<ba.range[2]),]
+dim(small.trees)
+table(small.trees$Species)
+
+small.trees.p <- aggregate(Basal.Area~Plot,data=small.trees,FUN=sum)
+head(small.trees.p)
+dim(small.trees.p)
+
+match.plots <- match(plot.list,small.trees.p$Plot)
+small.trees.p <- cbind(plot.list,small.trees.p[match.plots,])
+head(small.trees.p)
+
+small.trees.p$Basal.Area[which(is.na(small.trees.p$Basal.Area))] <- 0
+head(small.trees.p)
+
+colname <- paste('shrubTBA_',diam.range[1],'_',diam.range[2],sep='')
+pl$x <- small.trees.p$Basal.Area
+head(pl)
+names(pl)[grep('x',names(pl))] <- colname
+head(pl)
+pl[,c(colname,'Plot')]
+
+#### NOT USED FOR NOW
 #Pull SEJU data and create summary files
 
 seju.data<-get.seju.data(2013)
@@ -81,9 +223,6 @@ write.csv(seju.data.p, "data/seju_data_p.csv", row.names=F)
 write.csv(seju.data.s, "data/seju_data_s.csv", row.names=F)
 
 
-# %in% function
-which(indv.data$Species %in% c('PSEMEN'))
 
 
-ladderfuels=data.frame(c1=1:50)
-ladderfuels$c2 <- sapling
+
