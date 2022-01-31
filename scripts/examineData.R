749dc66451335eae3abf931e0d9b9cef292964bf
David Ackerly <dackerly@berkeley.edu>
2022-01-31 07:54:18 -0800

    added a bunch more misspellings to get.indv.data
    reran and exported allid, allidb
    
    updated examineData to output dups and trees missing in a middle year
    
    new/updated files in data:
    duplicates.csv
    mult-plots-species.csv

--- scripts/examineData.R
+++ scripts/examineData.R
@@ -14,18 +14,25 @@ years <- c(2013,2018,2019,2020)
 # etc.
 
 # Print dups for each year. And for now, remove dups before checking for other problems, e.g. moving between plots, species, etc.
+dups <- list()
 i=1
 for (i in 1:4)
 {
   print(years[i])
   nNum <- table(all.id[[i]]$Num)
   print(nNum[which(nNum>1)])
-  dups <- as.numeric(names(nNum[which(nNum>1)]))
-  if (length(dups)>0) all.id[[i]] <- all.id[[i]][-which(all.id[[i]]$Num %in% dups)]
+  dN <- as.numeric(names(nNum[which(nNum>1)]))
+  dups[[i]] <- all.id[[i]][which(all.id[[i]]$Num %in% dN),]
+  dups[[i]] <- dups[[i]][order(dups[[i]]$Num),]
+  if (length(dN)>0) all.id[[i]] <- all.id[[i]][-which(all.id[[i]]$Num %in% dN)]
 }
 
-for (i in 1:4) print(tail(sort(all.id[[i]]$Num)))
+dups[[1]]
+dups <- do.call(rbind,dups)
+dim(dups)
+write.csv(dups,'data/duplicates.csv')
 
+for (i in 1:4) print(tail(sort(all.id[[i]]$Num)))
 ### ONE BAD TAG: 39332 in 2018
 all.id[[2]][all.id[[2]]$Num==39332,]
 
@@ -59,6 +66,11 @@ for (i in 1:length(all.id))
 }
 head(allIndv)
 
+# Now check all names across all years
+head(allIndv[,c('S13','S18','S19','S20')])
+allNames <- sort(c(allIndv$S13,allIndv$S18,allIndv$S19,allIndv$S20))
+table(allNames)
+
 ## This data.frame may be quite useful for quickly identifying problem individuals. 
 
 # Now, let's see which numbers were assigned in different plots in different years. To do this we'll use the apply function which can apply a function either across the rows or columns of a matrix or dataframe. We'll make a new function which counts the number of unique entries. So if all three plot IDs were the same for a number, the function returns 1. If it's more than 1, indicates a problem
@@ -83,21 +95,17 @@ head(allIndv$nSp)
 # what did we find?
 table(allIndv$nSp)
 
-
-
 # Oh, interesting - individuals not assigned to any species? And some assigned to different species every year? Let's look at 0, 3
 probs <- which(allIndv$nSp %in% c(0,3,4))
 allIndv[probs,]
 
-# OK, almost 300 individuals with more than one Sp ID! We'll need to fix or exclude these. Here they are:
+# a bunch of them are indets seen only once in PPW1330, after the fire
+
+# OK, almost 250 individuals with more than one Sp ID! We'll need to fix or exclude these. Here they are:
 head(allIndv[multSp,1:9])
 
 ## how many are the same individuals
 table(allIndv$nP,allIndv$nSp)
-
-#make combined list
-allMults <- allIndv[union(multPlots,multSp),]
-write.csv(allMults[order(allMults$nSp,allMults$nP,decreasing = T),],'data/mult-plots-species.csv')
 
 ## which individuals are missing from 2018 and present in 2013 and 2019
 midNA <- function(x)
@@ -105,8 +113,17 @@ midNA <- function(x)
   if (!is.na(x[1]) & is.na(x[2]) & !is.na(x[3])) ret <- 1 else ret <- 0
   return(ret)
 }
-allIndv[apply(allIndv[,Pn[1:3]],1,midNA)==1,]
-allIndv[apply(allIndv[,Pn[2:4]],1,midNA)==1,]
+allIndv$NA18 <- apply(allIndv[,Pn[1:3]],1,midNA)
+m18 <- which(allIndv$NA18==1)
+allIndv$NA19 <- apply(allIndv[,Pn[2:4]],1,midNA)
+m19 <- which(allIndv$NA19==1)
+
+#make combined list
+allprobs <- union(multPlots,union(multSp,union(m18,m19)))
+allMults <- allIndv[allprobs,]
+head(allMults)
+write.csv(allMults[order(allMults$nSp,allMults$nP,decreasing = T),],'data/mult-plots-species.csv')
+
 
 ########
 # OLD CODE BELOW HERE
