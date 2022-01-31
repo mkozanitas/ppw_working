0da9f4c28cbe735b487ce11c9cb161233c9babce
David Ackerly <dackerly@berkeley.edu>
2022-01-30 16:11:45 -0800

    split into prepare and examine dada created Rdata objects for all indv.data

--- scripts/prepareData_v1_220121.R
+++ scripts/prepareData_v1_220121.R
@@ -12,8 +12,7 @@ id13 <- get.indv.data(year = 2013,branches=F)
 dim(id13)
 head(id13)
 head(sort(id13$Num))
-nNum <- table(id13$Num)
-nNum[which(nNum>1)]
+
 
 id13b <- get.indv.data(year = 2013,branches=T)
 dim(id13b)
@@ -48,8 +47,6 @@ id19b <- get.indv.data(year = 2019,branches=T)
 dim(id19b)
 head(id19b)
 
-# This one gives an error - skip for now
-
 plot.list.2020 <- get.plot(2020)
 # now remove plots not sampled
 plot.list.2020 <- plot.list.2020[-c(5,6,20,25,28,30,40:42,46,49,51:54)]
@@ -65,146 +62,7 @@ head(id20b)
 ## write out all the data to an RData file, so it can be read and analyzed without going back to github
 all.id <- list(id13,id18,id19,id20)
 all.idb <- list(id13b,id18b,id19b,id20b)
-saveRDS(all.id)
+saveRDS(all.id,'data/allid.Rdata')
+saveRDS(all.idb,'data/allidb.Rdata')
 
-## which numbers were missing in 2013
-head(sort(id13$Num))
-tail(sort(id13$Num))
 
-#set manually for quick and dirty
-maxnum.2013 <- 5153
-
-# Setting that one aside, which numbers are missing between 1001 and 5153 in 2013
-allNum13 <- sort(unique(id13$Num))
-length(allNum13) # number of missing tag numbers
-
-# Are there duplicated tags in any year?
-# put the three individual data dataframes into a list so that we can loop through them and do operations on them
-all.indv.data <- list(indv.data.2013,indv.data.2018,indv.data.2019,indv.data.2020)
-for (i in 1:length(all.indv.data)) 
-{
-  indv.data <- indv.data.2013
-  tagT <- table(indv.data.2013$Num)
-  print(c(i,tagT[which(tagT>1)]))
-}
-## APPARENTLY NO DUPLICATED TAGS IN ANY YEAR
-
-# Identify new tag numbers in 2018 that weren't in the 2013 data
-
-# Including ones less than 5154 that must have been out
-newtags <- sort(indv.data.2018$Num[!(indv.data.2018$Num %in% indv.data.2013$Num)])
-newtags
-(newtags.lt.maxnum <- newtags[newtags <= maxnum.2013])
-(newtags.gt.maxnum <- newtags[newtags > maxnum.2013])
-(maxnum.2018 <- max(indv.data.2018$Num))
-
-# Identify new tag numbers in 2019 that weren't in the 2018 data
-# Including ones less than 5154 that must have been out
-newtags.19 <- sort(indv.data.2019$Num[!(indv.data.2019$Num %in% indv.data.2018$Num)])
-newtags.19
-(newtags.19[newtags.19 <= maxnum.2018])
-(newtags.19[newtags.19 > maxnum.2018])
-(maxnum.19 <- max(indv.data.2019$Num))
-
-# Identify new tag numbers in 2020 that weren't in the 2019 data
-# Including ones less than 5154 that must have been out
-newtags.20 <- sort(indv.data.2020$Num[!(indv.data.2020$Num %in% indv.data.2019$Num)])
-newtags.20
-(newtags.20[newtags.20 <= maxnum.19])
-(newtags.20[newtags.20 > maxnum.19])
-
-## Now make a list of all numbers that appear across all years
-# The loop here makes this so it will run smoothly if additional years are added
-allNums <- c()
-
-# all.indv.data is the list made above, where each item is one years individual data. How many years does it have:
-length(all.indv.data)
-
-# make an empty variable, and then loop through the individual data files and append all the numbers end to end
-allNums <- c()
-for (i in 1:length(all.indv.data)) allNums <- c(allNums,all.indv.data[[i]]$Num)
-head(allNums)
-# now reduce to the unique ones
-allNums <- sort(unique(allNums))
-length(allNums)
-head(allNums)
-tail(allNums)
-
-# there are some numbers above that should be fixed. In the meantime, let's make a dataframe with all individuals across all years, which we can use to start checking problems
-allIndv <- data.frame(Num=allNums,P13=NA,P18=NA,P19=NA,P20=NA,S13=NA,S18=NA,S19=NA,S20=NA)
-
-# now use the match command to match up the plot for each number in each year, and assign it to the right row
-Pn <- c('P13','P18','P19','P20')
-Sn <- c('S13','S18','S19','S20')
-i=1
-for (i in 1:length(all.indv.data))
-{
-  y2a <- match(allIndv$Num,all.indv.data[[i]]$Num)
-  allIndv[,Pn[i]] <- all.indv.data[[i]]$Plot[y2a]
-  allIndv[,Sn[i]] <- all.indv.data[[i]]$Species[y2a]
-}
-head(allIndv)
-
-## This data.frame may be quite useful for quickly identifying problem individuals. For example, what numbers were present in plot 1345 in 2018 that were missed in 2013. Maybe that's the right number for the -999 individual
-allIndv[which(allIndv$P13=='PPW1345'),c('Num',Pn)]
-# Indv 2276 is missing in 2013 and present in later years. Is that the right number for the one with -999?
-
-# Now, let's see which numbers were assigned in different plots in different years. To do this we'll use the apply function which can apply a function either across the rows or columns of a matrix or dataframe. We'll make a new function which counts the number of unique entries. So if all three plot IDs were the same for a number, the function returns 1. If it's more than 1, indicates a problem
-lunique <- function(x) 
-{
-  nonNAx <- x[which(!is.na(x))]
-  return(length(unique(nonNAx)))
-}
-
-allIndv$nP <- apply(allIndv[,Pn],1,lunique)
-head(allIndv$nP)
-(multPlots <- which(allIndv$nP>1))
-
-# OK, 25 individuals that moved between plots! We need to fix those. Here they are:
-allIndv[multPlots,]
-
-# and which ones change species
-allIndv$nSp <- apply(allIndv[,Sn],1,lunique)
-head(allIndv$nSp)
-(multSp <- which(allIndv$nSp>1))
-
-# OK, almost 300 individuals with more than one Sp ID! We'll need to fix or exclude these. Here they are:
-allIndv[multSp,c('Num',Sn)]
-
-## how many are the same individuals
-table(allIndv$nP,allIndv$nSp)
-
-## which individuals are missing from 2018 and present in 2013 and 2019
-midNA <- function(x)
-{
-  if (!is.na(x[1]) & is.na(x[2]) & !is.na(x[3])) ret <- 1 else ret <- 0
-  return(ret)
-}
-allIndv[apply(allIndv[,Pn[1:3]],1,midNA)==1,]
-allIndv[apply(allIndv[,Pn[2:4]],1,midNA)==1,]
-
-########
-# OLD CODE BELOW HERE
-# 
-# # THe following lines identify which plots dupolicated numbers are in
-# 
-# # Check to see if tag numbers are duplicated in a particular year
-# length(unique(indv.data$Num)) # How many UNIQUE tag numbers
-# length(indv.data$Num) # How many TOTAL tag numbers
-# # Make tables with the duplicated number and which plot it's in
-# first <- data.frame(onetag=indv.data$Num[duplicated(indv.data$Num,fromLast=TRUE)],oneplot=indv.data$Plot[duplicated(indv.data$Num,fromLast=TRUE)])
-# second <- data.frame(twotag=indv.data$Num[duplicated(indv.data$Num)],twoplot=indv.data$Plot[duplicated(indv.data$Num)])
-# # Sort the tables so that the numbers are in the same order
-# first <- first[order(first$onetag),]
-# second <- second[order(second$twotag),]
-# # Bind the first and sxwecond dup tables
-# dups <- cbind(first,second)
-# # Add a check to see if duplicates are in the same plot
-# dups$sameplot <- apply(dups, 1, FUN=function(x) x[2] == x[4])
-# dups[order(dups$sameplot),]
-# 
-# 
-# grep("DUP",indv.data.2018$Notes)
-# indv.data.2018[1559,]
-# indv.data.2018[37,]
-# 
